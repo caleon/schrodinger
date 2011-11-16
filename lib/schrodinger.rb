ee34f6180cf89b328dfcf5abaff06ebb27b42adf
colin chun <caleon@gmail.com>
2011-11-15 16:52:37 -0800

    proper requirement of active_support core_ext's

--- lib/schrodinger.rb
+++ lib/schrodinger.rb
@@ -1,3 +1,6 @@
+require 'active_support/core_ext/module'
+require 'active_support/core_ext/array/wrap'
+
 require 'core_utilities'
 require 'schrodinger/schrodingers_cat'
 require 'schrodinger/core_ext'
